e9da8eda6619389980fc6acc3fc2b64f7d7d1fcb
LordlyCat <1607172507@qq.com>
2017-02-25 00:50:04 +0800

    write-1

--- ZhiHu/JS/person.js
+++ ZhiHu/JS/person.js
@@ -30,43 +30,126 @@ var change = document.querySelectorAll('.change');
 
 
     
-    revise[0].onclick = function () {
-        
-            change[0].innerHTML = '<input type="text">' +
-                        '<button>确定</button>' +
-                        '<button>取消</button>';
-            }
-
-
-    revise[1].onclick = function () {
-        
-            change[1].innerHTML = '<input type="text">' +
-                        '<button>确定</button>' +
-                        '<button>取消</button>';
-            }
-
-    revise[2].onclick = function () {
-        
-            change[2].innerHTML = '<input type="text">' +
-                        '<button>确定</button>' +
-                        '<button>取消</button>';
-            }
-            
-
-    revise[3].onclick = function () {
-        
-            change[3].innerHTML = '<input type="text">' +
-                        '<button>确定</button>' +
-                        '<button>取消</button>';
-            }
-
-    revise[4].onclick = function () {
-        
-            change[4].innerHTML = '<input type="text">' +
+revise[0].onclick = function () {
+    var oldtext = change[0].innerHTML;
+    change[0].innerHTML = '<input type="text">' +
                         '<button class = "submit">确定</button>' +
                         '<button>取消</button>';
-            }
-            
+    var submit = document.querySelector('.submit');
+    var input = document.querySelector('input');
+    var cancel = change[0].lastChild;
+
+    submit.onclick = function () {
+        var text = input.value;
+        if (text == '') {
+            change[0].innerHTML = oldtext;
+        }else {
+            change[0].innerHTML = text;
+        }
+               
+    }
+
+    cancel.onclick = function () {
+        
+    }
+        
+}
+
+revise[1].onclick = function () {
+    var oldtext = change[1].innerHTML;
+    change[1].innerHTML = '<input type="text">' +
+                        '<button class = "submit">确定</button>' +
+                        '<button>取消</button>';
+    var submit = document.querySelector('.submit');
+    var input = document.querySelector('input');
+    var cancel = change[1].lastChild;
+
+    submit.onclick = function () {
+        var text = input.value;
+        if (text == '') {
+            change[1].innerHTML = oldtext;
+        }else {
+            change[1].innerHTML = text;
+        }       
+    }
+
+    cancel.onclick = function () {
+        change[1].innerHTML = oldtext;
+    }
+        
+}
+
+revise[2].onclick = function () {
+    var oldtext = change[2].innerHTML;
+    change[2].innerHTML = '<input type="text">' +
+                        '<button class = "submit">确定</button>' +
+                        '<button>取消</button>';
+    var submit = document.querySelector('.submit');
+    var input = document.querySelector('input');
+    var cancel = change[2].lastChild;
+
+    submit.onclick = function () {
+        var text = input.value;
+        if (text == '') {
+            change[2].innerHTML = oldtext;
+        }else {
+            change[2].innerHTML = text;
+        }       
+    }
+
+    cancel.onclick = function () {
+        change[2].innerHTML = oldtext;
+    }
+        
+}
+3
+revise[3].onclick = function () {
+    var oldtext = change[3].innerHTML;
+    change[3].innerHTML = '<input type="text">' +
+                        '<button class = "submit">确定</button>' +
+                        '<button>取消</button>';
+    var submit = document.querySelector('.submit');
+    var input = document.querySelector('input');
+    var cancel = change[3].lastChild;
+
+    submit.onclick = function () {
+        var text = input.value;
+        if (text == '') {
+            change[3].innerHTML = oldtext;
+        }else {
+            change[3].innerHTML = text;
+        }       
+    }
+
+    cancel.onclick = function () {
+        change[3].innerHTML = oldtext;
+    }
+        
+}
+
+revise[4].onclick = function () {
+    var oldtext = change[4].innerHTML;
+    change[4].innerHTML = '<input type="text">' +
+                        '<button class = "submit">确定</button>' +
+                        '<button>取消</button>';
+    var submit = document.querySelector('.submit');
+    var input = document.querySelector('input');
+    var cancel = change[4].lastChild;
+
+    submit.onclick = function () {
+        var text = input.value;
+        if (text == '') {
+            change[4].innerHTML = oldtext;
+        }else {
+            change[4].innerHTML = text;
+        }       
+    }
+
+    cancel.onclick = function () {
+        change[4].innerHTML = oldtext;
+    }
+        
+}
 
 /*function cat (a,b) {
      a.onclick = function () {
@@ -77,9 +160,4 @@ var change = document.querySelectorAll('.change');
         }
 }*/
 
-var submit = document.querySelectorAll('.submit');
-
-submit[0].onclick = function () {
-    alert('233');
-}
-
+/*var submit = document.querySelectorAll('.submit');*/
